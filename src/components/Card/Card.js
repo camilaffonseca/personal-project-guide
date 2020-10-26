ce791ca81a7c2670f78302a1cd3090b348ecb8c4
camilaffonseca <camilaffonseca.ang7@gmail.com>
2020-10-26 03:30:06 -0300

    feat(components): upgraded style in Title component / finished Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components'
 
+import { DESKTOP_BREAKPOINT } from 'helpers/constants'
+
 const Card = styled.div`
-  background-color: red;
+  background-color: var(--color-background-secondary);
+  border: 1px solid var(--color-border-primary);
+  border-radius: 6px;
+  width: 95vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 1.5rem;
+  padding: 2.6rem;
+
+  @media (min-width: ${DESKTOP_BREAKPOINT - 150}px) {
+    width: 85vw;
+  }
+  @media (min-width: ${DESKTOP_BREAKPOINT + 300}px) {
+    width: 75vw;
+  }
 `
 
 export default Card
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 const Title = styled.p`
   color: var(--color-text-primary);
   font-size: 3.9rem;
-  margin: 2.4rem 0 1.6rem;
+  margin-bottom: 2.4rem;
   font-weight: 600;
   font-family: 'Montserrat', sans-serif;
+  cursor: default;
 `
 
 export default Title
