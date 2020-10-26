9fc69c207ac695964a6b8308353e3c8d6edbe3dc
camilaffonseca <camilaffonseca.ang7@gmail.com>
2020-10-26 00:16:36 -0300

    refactor(app): refactored App to styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Title from 'components/Title'
 import Container from 'components/Container'
@@ -7,19 +8,22 @@ import pythonLogo from 'images/python-logo.png'
 import reactLogo from 'images/react-logo.png'
 import javascriptLogo from 'images/javascript-logo.png'
 
-import './App.css'
-
-function App() {
+const App = () => {
   return (
     <>
       <Container>
-        <img className='image' src={pythonLogo} alt="Python Logo"/>
-        <img className='image' src={reactLogo} alt="React Logo"/>
-        <img className='image' src={javascriptLogo} alt="JavaScript Logo"/>
+        <Image className='image' src={pythonLogo} alt='Python Logo' />
+        <Image className='image' src={reactLogo} alt='React Logo' />
+        <Image className='image' src={javascriptLogo} alt='JavaScript Logo' />
       </Container>
-      <Title>Guia de Projeto</Title>
+      <Title>Guia de Projetos</Title>
     </>
   )
 }
+
+const Image = styled.img`
+  width: calc(15vw + 5rem);
+  max-width: 15rem;
+`
 
 export default App
