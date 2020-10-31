47c0934aaa1f31ade5e95f2c0e18a101c49016b4
camilaffonseca <camilafonseca.ang7@gmail.com>
2020-10-31 03:22:30 -0300

    feat(components): added theme icons and created ToggleThemeButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
@@ -11,31 +11,41 @@ import Happy from 'routes/project/Happy'
 import ToDos from 'routes/project/ToDos'
 import LearningJS from 'routes/project/LearningJS'
 import MooDevProjects from 'routes/project/MooDevProjects'
+import ToggleThemeButton from 'components/ToggleThemeButton'
 
 import GlobalStyle from 'styles'
 
 import { darkTheme, lightTheme } from 'theme'
 
-const App = () => (
-  <ThemeProvider theme={darkTheme}>
-    <GlobalStyle />
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
+const App = () => {
+  const [currentTheme, setCurrentTheme] = useState('light')
 
-        <Route exact path='/project/personal-project-guide' component={PersonalProjectGuide} />
-        <Route exact path='/project/learning-python' component={LearningPython} />
-        <Route exact path='/project/jshunt' component={JSHunt} />
-        <Route exact path='/project/react-fundamentals' component={ReactFundamentals} />
-        <Route exact path='/project/happy' component={Happy} />
-        <Route exact path='/project/to-dos' component={ToDos} />
-        <Route exact path='/project/learning-js' component={LearningJS} />
-        <Route exact path='/project/moodev-projects' component={MooDevProjects} />
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light')
+  }
 
-        <Redirect to='/' />
-      </Switch>
-    </Router>
-  </ThemeProvider>
-)
+  return (
+    <ThemeProvider theme={currentTheme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <ToggleThemeButton currentTheme={currentTheme} toggleThemeCallback={toggleTheme} />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+
+          <Route exact path='/project/personal-project-guide' component={PersonalProjectGuide} />
+          <Route exact path='/project/learning-python' component={LearningPython} />
+          <Route exact path='/project/jshunt' component={JSHunt} />
+          <Route exact path='/project/react-fundamentals' component={ReactFundamentals} />
+          <Route exact path='/project/happy' component={Happy} />
+          <Route exact path='/project/to-dos' component={ToDos} />
+          <Route exact path='/project/learning-js' component={LearningJS} />
+          <Route exact path='/project/moodev-projects' component={MooDevProjects} />
+
+          <Redirect to='/' />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  )
+}
 
 export default App
