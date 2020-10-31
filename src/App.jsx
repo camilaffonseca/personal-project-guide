33ed63875cf8ae3472919cd94aa6bc8d4c3dc288
camilaffonseca <camilafonseca.ang7@gmail.com>
2020-10-31 01:16:05 -0300

    feat(app): added theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
 import Home from 'routes/Home'
 import PersonalProjectGuide from 'routes/project/PersonalProjectGuide'
@@ -11,23 +12,30 @@ import ToDos from 'routes/project/ToDos'
 import LearningJS from 'routes/project/LearningJS'
 import MooDevProjects from 'routes/project/MooDevProjects'
 
+import GlobalStyle from 'styles'
+
+import { darkTheme, lightTheme } from 'theme'
+
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={Home} />
+  <ThemeProvider theme={darkTheme}>
+    <GlobalStyle />
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
 
-      <Route exact path='/project/personal-project-guide' component={PersonalProjectGuide} />
-      <Route exact path='/project/learning-python' component={LearningPython} />
-      <Route exact path='/project/jshunt' component={JSHunt} />
-      <Route exact path='/project/react-fundamentals' component={ReactFundamentals} />
-      <Route exact path='/project/happy' component={Happy} />
-      <Route exact path='/project/to-dos' component={ToDos} />
-      <Route exact path='/project/learning-js' component={LearningJS} />
-      <Route exact path='/project/moodev-projects' component={MooDevProjects} />
+        <Route exact path='/project/personal-project-guide' component={PersonalProjectGuide} />
+        <Route exact path='/project/learning-python' component={LearningPython} />
+        <Route exact path='/project/jshunt' component={JSHunt} />
+        <Route exact path='/project/react-fundamentals' component={ReactFundamentals} />
+        <Route exact path='/project/happy' component={Happy} />
+        <Route exact path='/project/to-dos' component={ToDos} />
+        <Route exact path='/project/learning-js' component={LearningJS} />
+        <Route exact path='/project/moodev-projects' component={MooDevProjects} />
 
-      <Redirect to='/' />
-    </Switch>
-  </Router>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  </ThemeProvider>
 )
 
 export default App
