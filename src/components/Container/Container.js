463b8de382a3dea52965086e645460f14ac065b4
camilaffonseca <camilafonseca.ang7@gmail.com>
2020-11-02 04:41:33 -0300

    feat(container): added responsive css layout

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -4,13 +4,20 @@ import { DESKTOP_BREAKPOINT } from 'helpers/constants'
 
 const Container = styled.div`
   width: 87vw;
-  max-width: 580px;
-  margin: 7.2rem 0;
+  margin: calc(7.2rem + 15vw) 0 7.2rem 0;
+  max-width: 100vw;
   display: flex;
   justify-content: space-around;
 
+  @media (min-width: 210px) {
+    margin: calc(7.2rem + 6rem) 0 7.2rem 0;
+  }
   @media (min-width: ${DESKTOP_BREAKPOINT}px) {
     width: calc(53vw + 5rem);
+    max-width: 75rem;
+  }
+  @media (min-width: ${DESKTOP_BREAKPOINT + 300}px) {
+    margin: 7.2rem 0;
   }
 `
 
